50c226ffb4e7ffc38ca144790e181a43aa4278f4
kzzzzztein <kylezsantoz2004@gmail.com>
2026-08-19 22:42:36 +0800

    Delete playstation-sale-tracker directory

--- playstation-sale-tracker/packages/shared/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./currency.js";
-export * from "./strings.js";
-
-export const REGIONS_META = [
-  { code: "us", name: "United States", currency: "USD", currencySymbol: "$", flagEmoji: "🇺🇸", storeLocale: "en-us" },
-  { code: "sg", name: "Singapore", currency: "SGD", currencySymbol: "S$", flagEmoji: "🇸🇬", storeLocale: "en-sg" },
-  { code: "hk", name: "Hong Kong", currency: "HKD", currencySymbol: "HK$", flagEmoji: "🇭🇰", storeLocale: "en-hk" },
-  { code: "tr", name: "Turkey", currency: "TRY", currencySymbol: "₺", flagEmoji: "🇹🇷", storeLocale: "tr-tr" },
-] as const;
