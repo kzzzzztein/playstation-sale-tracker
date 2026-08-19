7970b1e4075a2b3d4b7fca9c7dc2e3dee285a2b5
kzzzzztein <kylezsantoz2004@gmail.com>
2026-08-19 22:55:35 +0800

    Delete packages directory

--- packages/database/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./client.js";
-export * from "./schema.js";
-export * from "./queries/regions.js";
-export * from "./queries/games.js";
-export * from "./queries/history.js";
-export * from "./queries/ingestion.js";
-export * from "./queries/admin.js";
